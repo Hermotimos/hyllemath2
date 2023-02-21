45199b75adb97f84e0ff68eea24a23267f8a23b8
Łukasz Kozicki <lukas.kozicki@gmail.com>
2023-02-21 21:38:39 +0100

    WIP sql for Character etc.

---
+++ prsp/forms.py
@@ -0,0 +1,2 @@
+
+#  TODO: enforce length limit of TEXT columns (CharField, TextField), otherwise there's no control!
--- prsp/models.py
+++ prsp/models.py
@@ -1,3 +1,31 @@
-from django.db import models
+from django.db.models import (
+    CASCADE, PROTECT, SET_NULL, EnumField, TextChoices, Model,
+    AutoField, CharField, ForeignKey, Index,
+    IntegerField, TextField
+)
 
-# Create your models here.
+
+
+"""
+
+chatGPT
+
+1. use direct imports of field types and other features and never use "models." syntax; use these:
+from django.db.models import (
+    CASCADE, PROTECT, SET_NULL, EnumField, TextChoices, Model,
+    AutoField, CharField, ForeignKey, Index,
+    IntegerField, TextField
+)
+
+2. for Gender enum use EnumField and declare its class withing FirstName class
+
+3. declare models as unmanaged
+
+4. create __str__ methods with self.title whenever possible, elsewhere guess and add comment "check it!"
+
+5. create get_absolute_url methods with the following schema, replacing MODELNAME with lowercase name of the model
+    return reverse('prsp:MODELNAME', kwargs={'MODELNAME_id' : self.id})
+
+paste
+
+"""
